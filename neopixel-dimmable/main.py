c9b7ed89b9788dce8ce55a656baa16e55e37d251
Stéphane Bouchard <sbouchard25@gmail.com>
2022-10-11 23:07:43 -0400

    Update with the protoboard design

--- neopixel-dimmable/main.py
+++ neopixel-dimmable/main.py
@@ -11,8 +11,8 @@ indigo = (75, 0, 130)
 violet = (138, 43, 226)
 colors = (red, orange, yellow, green, blue, indigo, violet)
 
-brightness = machine.ADC(26)
-color = machine.ADC(27)
+brightness = machine.ADC(27)
+color = machine.ADC(26)
 
 numpix = 30
 strip = Neopixel(numpix, 0, 28, "GRB")
@@ -26,9 +26,6 @@ color_factor = (len(colors)-1) / (65535)
 onboard_led = machine.Pin(25, machine.Pin.OUT)
 onboard_led.value(0)
 
-external_led = machine.Pin(15, machine.Pin.OUT)
-external_led.value(0)
-
 timer=machine.Timer()
 timer2=machine.Timer()
 timer3=machine.Timer()
@@ -36,17 +33,17 @@ timer3=machine.Timer()
 def mycallback(t):
     print('mycallback')
     timer2.deinit()
-    external_led.value(0)
+    onboard_led.value(0)
 
 def mycallback2(t):
     if (Shutdown_Strip == 0):
         print('mycallback2')
         brightness_value = brightness.read_u16() * brightness_factor
         color_value = round(color.read_u16() * color_factor)
-    #     print('Brightness: %s' % brightness_value)
-    #     print(color_value)
-    #     print(round(color_value))
-    #     print(colors[color_value-1])
+#        print('Brightness: %s' % brightness_value)
+#        print(color_value)
+#        print(round(color_value))
+#        print(colors[color_value-1])
         strip.brightness(brightness_value)
         strip.fill(colors[round(color_value)])
         strip.show()
@@ -55,7 +52,7 @@ def mycallback3(t):
     print('mycallback3')
     Shutdown_Strip = 1
     timer2.deinit()
-    external_led.value(0)
+    onboard_led.value(0)
     timer.deinit()
     strip.clear()
     strip.show()
@@ -64,9 +61,6 @@ def mycallback3(t):
 def Pb_Switch_INT(pin):         # PB_Switch Interrupt handler
     global Pb_Switch_State      # reference the global variable
     global Shutdown_Strip
-#     global timer=machine.Timer()
-#     global timer2=machine.Timer()
-#     global timer3=machine.Timer()
     
     Shutdown_Strip = 0
     Pb_Switch.irq(handler = None) # Turn off the handler while it is executing
@@ -74,9 +68,8 @@ def Pb_Switch_INT(pin):         # PB_Switch Interrupt handler
     if (Pb_Switch.value() == 1) and (Pb_Switch_State == 0):  # Pb_Switch is active (High) and Pb_Switch State is currently Low
         Pb_Switch_State = 1     # Update current state of switch
         onboard_led.value(1)    # Do required action here
-        external_led.value(1)
         timer.deinit()
-        timer.init(period=5000, mode=machine.Timer.ONE_SHOT, callback=mycallback)
+        timer.init(period=10000, mode=machine.Timer.ONE_SHOT, callback=mycallback)
         timer2.deinit()
         timer2.init(period=100, mode=machine.Timer.PERIODIC, callback=mycallback2)
         timer3.deinit()
@@ -85,7 +78,6 @@ def Pb_Switch_INT(pin):         # PB_Switch Interrupt handler
 
     elif (Pb_Switch.value() == 0) and (Pb_Switch_State == 1): # Pb_Switch is not-active (Low) and Pb_Switch State is currently High
         Pb_Switch_State = 0     # Update current state of switch
-        onboard_led.value(0)    # Do required action here
         print("OFF \n")         # Do required action here
         timer3.deinit()
 
@@ -93,8 +85,7 @@ def Pb_Switch_INT(pin):         # PB_Switch Interrupt handler
 
 
 #Creat an 'object' for our Pb_Switch change of state
-# Pb_Switch = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
-Pb_Switch = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
+Pb_Switch = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
 #Setup the Interrupt Request Handling for Pb_Switch change of state
 Pb_Switch.irq(trigger = machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler = Pb_Switch_INT)
 
@@ -106,5 +97,6 @@ Shutdown_Strip = 0
 
 while True:
     utime.sleep(.0001)
+
 
 
